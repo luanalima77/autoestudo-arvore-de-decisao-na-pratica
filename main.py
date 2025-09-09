9ba958646effc584383b700ddaba18aa640ec370
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-09 08:26:32 -0300

    feat: criar árvore de decisão usando DecisionTreeClassifier

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 import pandas as pd
 from sklearn import datasets
 iris = datasets.load_iris()
+
+#Criar árvore de decisão.
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+x = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+model = DecisionTreeClassifier(criterion="entropy", random_state=42)
+
+

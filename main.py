4434bdd278ca77e47f7fb9cac931074530012f46
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-09 08:44:52 -0300

    feat: criar árvore de decisão e avaliar as previsões

--- main.py
+++ main.py
@@ -10,5 +10,24 @@ x = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 model = DecisionTreeClassifier(criterion="entropy", random_state=42)
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
 
+#Avaliar previsões realizadas.
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test, y_pred)
 
+def accuracy(yt, yp):
+    return (yp==yt).mean()
+
+print(accuracy(y_test, y_pred))
+
+#Imprimir a matriz de confusão.
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+confusion_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(confusion_matrix)
+plt.xlabel("Previsto")
+plt.ylabel("Real")
+plt.show()
